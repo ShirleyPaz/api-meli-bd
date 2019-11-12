c81491f4fd1862731a13cf90fac7b94964728ff6
shirleypaz <shirley.pazz@gmail.com>
2019-11-12 19:17:10 -0300

    mogoose instalado e db conectado

--- src/app.js
+++ src/app.js
@@ -1,24 +1,36 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/reprograma", {
+  useNewUrlParser: true
+});
+
+let db = mongoose.connection;
+
+db.on("error", console.log.bind(console,"connnection error:"));
+db.once("open", function() {
+  console.log('conexão feita com sucesso');
+})
 
 //rotas
-const index = require("./routes/index")
-const alunas = require("./routes/alunasRoute")
-const professoras = require("./routes/professorasRoute")
+const index = require("./routes/index");
+const alunas = require("./routes/alunasRoute");
+const professoras = require("./routes/professorasRoute");
 
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  next()
-})
+  );
+  next();
+});
 
-app.use("/", index)
-app.use("/alunas", alunas)
-app.use("/professoras", professoras)
+app.use("/", index);
+app.use("/alunas", alunas);
+app.use("/professoras", professoras);
 
-module.exports = app
+module.exports = app;
